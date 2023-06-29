b7f07121973abb15acefdeb638f199e80dcbc3f3
for-glory <forglory0905@outlook.com>
2023-06-29 00:03:04 -0700

    add mock scores

---
+++ src/app/mock-scores.ts
@@ -0,0 +1,11 @@
+import { Score } from './score';
+
+export const SCORES: Score[] = [
+  ["Pakistan", 23],
+  ["Pakistan", 127],
+  ["India", 3],
+  ["India", 71],
+  ["Australia", 31],
+  ["India", 22],
+  ["Pakistan", 81],
+];
