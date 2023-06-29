957c706072aed2223512965c6753833b1bb7d967
for-glory <forglory0905@outlook.com>
2023-06-29 00:05:55 -0700

    add app module and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SourceSelectorComponent } from './source-selector/source-selector.component';
+import { ScoreSearchComponent } from './score-search/score-search.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent,
+    SourceSelectorComponent,
+    ScoreSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
